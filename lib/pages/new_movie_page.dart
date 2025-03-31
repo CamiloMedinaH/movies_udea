30d10ea2b5172c5eca85b9c4440eaac0a2dcc44f
CamiloMedinaH <camilo.medinah@udea.edu.co>
2025-03-31 18:54:10 -0500

    Marzo 31: creacion de paginas

---
+++ lib/pages/new_movie_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewMoviePage extends StatefulWidget {
+  const NewMoviePage({super.key});
+
+  @override
+  State<NewMoviePage> createState() => _NewMoviePageState();
+}
+
+class _NewMoviePageState extends State<NewMoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/recovey_password.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecoveyPassword extends StatefulWidget {
+  const RecoveyPassword({super.key});
+
+  @override
+  State<RecoveyPassword> createState() => _RecoveyPasswordState();
+}
+
+class _RecoveyPasswordState extends State<RecoveyPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
