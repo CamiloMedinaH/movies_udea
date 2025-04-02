63dc255a0ae4a69a23fcc92c5a708d5746eadb69
CamiloMedinaH <camilo.medinah@udea.edu.co>
2025-04-02 17:55:23 -0500

    Abril 2: configuracion de las primeras paginas: splash, sign_in y recovey_password .

--- lib/pages/recovey_password.dart
+++ lib/pages/recovey_password.dart
@@ -8,8 +8,46 @@ class RecoveyPassword extends StatefulWidget {
 }
 
 class _RecoveyPasswordState extends State<RecoveyPassword> {
+
+  final _email = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(
+                    image: AssetImage("assets/images/logo.png"),
+                    width: 150,
+                    height: 150,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: _email,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email),
+                        labelText: "Correo electronico"),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: null,
+                    child: const Text("Recuperar contraseña"),
+                  ),
+                ],
+              ),
+            )
+        ),
+      ),
+    );
   }
 }
--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_udea/pages/recovey_password.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -8,8 +9,104 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+
+  bool _isPasswordObscure = true;
+  final _email = TextEditingController();
+  final _password = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(
+                    image: AssetImage("assets/images/logo.png"),
+                    width: 150,
+                    height: 150,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: _email,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email),
+                        labelText: "Correo electronico"),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: _password,
+                    obscureText: _isPasswordObscure,
+                    decoration:  InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(_isPasswordObscure
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordObscure = !_isPasswordObscure;
+                            });
+                          },
+                        ),
+                        labelText: "Contraseña"),
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: null,
+                    child: const Text("Iniciar sesion"),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    onPressed: (){},
+                    child:const Text("Registrarse"),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RecoveyPassword()
+                        ),
+                      );
+                    },
+                    child:const Text("Olvidaste tu constraseña?"),
+                  ),
+                ],
+              ),
+            )
+          ),
+      ),
+    );
   }
 }
